1d863ce5a1f56c46daf53e8808f71093ad6a602f
Marcos Veliz <marcus.devprojects@gmail.com>
2026-05-03 20:38:57 -0300

    feat: add CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
-import express from 'express'
-import productsRouter from './src/routes/products.routes.js'
+import express from "express";
+import cors from "cors";
+import productsRouter from "./src/routes/products.routes.js";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
-app.use('/api/products', productsRouter)
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'API funcionando' })
-})
+app.use("/api/products", productsRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando" });
+});
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`)
-})
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
