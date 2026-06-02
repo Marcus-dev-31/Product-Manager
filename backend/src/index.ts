18a8bd7c71c6055876e54c3e34f860b91dce136b
Marcos Veliz <marcus.devprojects@gmail.com>
2026-06-02 18:58:56 -0300

    fix: add custom domain to CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,39 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import productsRouter from './routes/products.routes.js'
-import authRoutes from './routes/auth.routes.js'
-import { businessRouter } from './routes/business.routes.js'
-import { teamRouter } from './routes/team.routes.js'
-import { emailRouter } from './routes/email.routes.js'
-import { contactRouter } from './routes/contact.routes.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import productsRouter from "./routes/products.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import { businessRouter } from "./routes/business.routes.js";
+import { teamRouter } from "./routes/team.routes.js";
+import { emailRouter } from "./routes/email.routes.js";
+import { contactRouter } from "./routes/contact.routes.js";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://precify-eta.vercel.app']
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://precify-eta.vercel.app",
+      "https://productmanager.marcusveliz.dev",
+    ],
+  }),
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/products', productsRouter)
-app.use('/api/auth', authRoutes)
-app.use('/api/business', businessRouter)
-app.use('/api/team', teamRouter)
-app.use('/api/email', emailRouter)
-app.use('/api/contact', contactRouter)
+app.use("/api/products", productsRouter);
+app.use("/api/auth", authRoutes);
+app.use("/api/business", businessRouter);
+app.use("/api/team", teamRouter);
+app.use("/api/email", emailRouter);
+app.use("/api/contact", contactRouter);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'API funcionando' })
-})
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
-})
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
